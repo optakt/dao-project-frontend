27dca043d6bc3f7494f8c7630b88a12fe717a6c9
Diego Escobar <diego.netsolutions@gmail.com>
2022-06-14 10:10:56 +0300

    added Support to Markdown

--- src/components/ItemPoll/ItemPoll.jsx
+++ src/components/ItemPoll/ItemPoll.jsx
@@ -1,15 +1,21 @@
-
+import ReactMarkdown from 'react-markdown'
 import './ItemPoll.scss'
 
 function ItemPoll({ poll }) {
+
+  console.log({ poll })
   return (
 
     <div className='itemPollWrapper'>
+      <div className='itemPollAuthor'>
+        {poll.proposal}
+      </div>
       <div className='itemPollTitle'>
         {poll.title}
       </div>
       <div className='itemPollDescription'>
-        {poll.description}
+        <ReactMarkdown children={poll.body} />
+
       </div>
     </div>
 
